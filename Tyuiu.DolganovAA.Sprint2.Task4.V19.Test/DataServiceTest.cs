039629ed999810c024dacb044e65ebedcc6952ab
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2024-10-16 19:31:17 +0300

    Task4

---
+++ Tyuiu.DolganovAA.Sprint2.Task4.V19.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.DolganovAA.Sprint2.Task4.V19.Lib;
+namespace Tyuiu.DolganovAA.Sprint2.Task4.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Valid()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 2;
+            Assert.AreEqual(0.874, ds.Calculate(x, y));
+        }
+    }
+}
